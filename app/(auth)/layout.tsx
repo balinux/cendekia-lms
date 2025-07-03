cbcb5a413e7e7646605206f5bd508f5dbf345942
balinux <ryojuniyantara@gmail.com>
2025-07-04 06:16:31 +0800

    add: term and condition

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
+import file from "@/public/file.svg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
@@ -16,7 +18,14 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
                 Back
             </Link>
             <div className="flex w-full max-w-sm flex-col gap-6">
+                <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                    <Image src={file} alt="Logo" width={24} height={24} />
+                    Cendekia
+                </Link>
                 {children}
+                <div className="text-balance text-xs text-center text-muted-foreground">
+                    By clicking continue, you agree to our <span className="hover:text-primary hover:underline cursor-pointer">Terms of Service</span> and <span className="hover:text-primary hover:underline cursor-pointer">Privacy Policy</span>
+                </div>
             </div>
         </div>
     )
