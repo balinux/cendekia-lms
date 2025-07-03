8d8d64712537687599082f435a016133ef5e9516
balinux <ryojuniyantara@gmail.com>
2025-07-04 05:05:23 +0800

    add: dark mode setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <h1>Hello bro</h1>
+    <ThemeToggle/>
+    </>
+    
   );
 }
