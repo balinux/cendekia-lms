d67e569a06cb9677c1c8f69308bb336ad2bbb41f
balinux <ryojuniyantara@gmail.com>
2025-07-04 04:52:58 +0800

    add: github icon

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
 
 export default function LoginPage() {
     return (
@@ -8,6 +9,9 @@ export default function LoginPage() {
                 <CardTitle className="text-xl">Welcome back!</CardTitle>
                 <CardDescription>Login with GitHub email account</CardDescription>
             </CardHeader>
+            <CardContent>
+                <Button className="w-full" variant="outline"> <GithubIcon className="size-4" /> Continue with GitHub</Button>
+            </CardContent>
         </Card>
     )
 }
