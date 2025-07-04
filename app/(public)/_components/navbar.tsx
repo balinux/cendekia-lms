46b2cac891d1a9781e3d60cde3eb734224d55640
balinux <ryojuniyantara@gmail.com>
2025-07-04 19:02:41 +0800

    add; logout button

--- app/(public)/_components/navbar.tsx
+++ app/(public)/_components/navbar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "@/public/file.svg";
 import Image from "next/image";
 import { ThemeToggle } from "@/components/theme-toggle";
-
+import { authClient } from "@/lib/auth-client";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Loader2, LogOut } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 interface NavigationItem {
     name: string;
     href: string;
@@ -16,6 +22,19 @@ const navigationItems: NavigationItem[] = [
 ]
 
 export default function Navbar() {
+    const { data: session, isPending } = authClient.useSession();
+
+    const router = useRouter();
+    async function signOut() {
+        await authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push("/login"); // redirect to login page
+                    toast.success("Logout success")
+                },
+            },
+        });
+    }
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
             <div className=" container flex  min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
@@ -34,8 +53,31 @@ export default function Navbar() {
                         ))}
                     </div>
 
-                    <div className="flex items-center space-x-2">
+                    <div className="flex items-center space-x-4">
                         <ThemeToggle />
+                        {isPending ? null : session ? (
+                            <Button variant="outline" onClick={signOut} disabled={isPending}>
+                                {isPending ? (
+                                    <Loader2 className="mr-2 size-4 animate-spin" />
+                                ) : (
+                                    <LogOut className="size-4" />
+                                )}
+                                Logout
+                            </Button>
+                        ) : (
+                            <>
+                                <Link href="/login" className={buttonVariants({
+                                    variant: "secondary",
+                                })}>
+                                    Login
+                                </Link>
+                                <Link href="/register" className={buttonVariants({
+                                    variant: "default",
+                                })}>
+                                    Get Started
+                                </Link>
+                            </>
+                        )}
                     </div>
 
 
