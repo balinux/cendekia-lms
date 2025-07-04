efa4bcc6b5521e1b095f782f2051f094cd0ab423
balinux <ryojuniyantara@gmail.com>
2025-07-04 09:13:16 +0800

    feat: implement database and authentication with better auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 export default function LoginPage() {
+
+    const [isGithubPending, startGithubTransition] = useTransition()
+
+    async function signInWithGithub() {
+        startGithubTransition(async () => {
+            await authClient.signIn.social({
+                provider: 'github',
+                callbackURL: '/',
+                fetchOptions: {
+                    onSuccess: () => {
+                        toast.success("Login success")
+                    },
+                    onError: () => {
+                        toast.error("Login failed")
+                    }
+                }
+            })
+        })
+    }
     return (
         <Card>
             <CardHeader>
@@ -12,7 +36,18 @@ export default function LoginPage() {
                 <CardDescription>Login with GitHub email account</CardDescription>
             </CardHeader>
             <CardContent className=" flex flex-col gap-4">
-                <Button className="w-full" variant="outline"> <GithubIcon className="size-4" /> Continue with GitHub</Button>
+                <Button className="w-full" variant="outline" onClick={signInWithGithub} disabled={isGithubPending}> 
+                    {isGithubPending?(
+                        <>
+                        <Loader className="mr-2 size-4 animate-spin" />
+                        <span>Loading...</span>
+                        </>
+                    ):(
+                        <>
+                        <GithubIcon className="size-4" /> Continue with GitHub
+                        </>
+                    )}
+                </Button>
 
                 {/* divider */}
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:itmes-center after:border-t after:border-border">
