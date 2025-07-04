308bdd5be9879dc548c6ba803e66a4aa189cbb84
balinux <ryojuniyantara@gmail.com>
2025-07-04 11:23:24 +0800

    feat: implement email auth with otp

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins"
+import { resend } from "./resend";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -12,5 +14,21 @@ export const auth = betterAuth({
             clientId:env.GITHUB_CLIENT_ID,
             clientSecret:env.GITHUB_CLIENT_SECRET,            
         }
-    }
+    },
+    plugins:[
+        emailOTP({ 
+            async sendVerificationOTP({ email, otp, type}) { 
+                // Implement the sendVerificationOTP method to send the OTP to the user's email address
+                const { data, error } = await resend.emails.send({
+                    from: 'Cendekia <onboarding@resend.dev>',
+                    to: [email],
+                    subject: 'Cendekia - Verify your email',
+                    // react: EmailTemplate({ firstName: 'John' }),
+                    html:`<p>Your verification code is:</p>
+                    <p>${otp}</p>
+                    <p>Thank you for using Cendekia</p>`
+                  });
+            }, 
+    }) 
+    ]
 })
