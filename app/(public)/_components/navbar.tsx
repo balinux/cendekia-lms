34cdfa718aad6c11c39b341774d2a7ae6c75f7a5
balinux <ryojuniyantara@gmail.com>
2025-07-04 18:45:28 +0800

    add: navigation item

--- app/(public)/_components/navbar.tsx
+++ app/(public)/_components/navbar.tsx
@@ -1,18 +1,48 @@
 import Link from "next/link";
 import Logo from "@/public/file.svg";
 import Image from "next/image";
+import { ThemeToggle } from "@/components/theme-toggle";
+
+interface NavigationItem {
+    name: string;
+    href: string;
+}
+
+const navigationItems: NavigationItem[] = [
+    { name: 'home', href: '/' },
+    { name: 'courses', href: '/courses' },
+    { name: 'about', href: '/about' },
+    { name: 'contact', href: '/contact' },
+]
 
 export default function Navbar() {
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
-            <div className="container flex items-center justify-between py-4">
-                <div className=" container flex  min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
-                    <Link href="/" className="flex items-center space-x-2 mr-4">
-                        <Image src={Logo} alt="Logo" width={24} height={24} />
-                        <span className="font-bold">Cendekia</span>
-                    </Link>
-                </div>
+            <div className=" container flex  min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+                <Link href="/" className="flex items-center space-x-2 mr-4">
+                    <Image src={Logo} alt="Logo" width={24} height={24} />
+                    <span className="font-bold">Cendekia</span>
+                </Link>
+
+                {/* desktop navigation */}
+                <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+                    <div className="flex items-center space-x-2">
+                        {navigationItems.map((item) => (
+                            <Link key={item.name} href={item.href} className="text-sm font-medium transition-colors hover:text-primary">
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                        <ThemeToggle />
+                    </div>
+
+
+                </nav>
             </div>
+
+
         </header>
     );
 }
