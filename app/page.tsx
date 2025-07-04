f17f881913c257952fcbfd51501bc6f1783e46f9
balinux <ryojuniyantara@gmail.com>
2025-07-04 09:56:12 +0800

    update: protect login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (!session) {
+    return <div>Not authenticated go to <Link href="/login">login</Link></div>
+  }
+
   return (
-    <>
-      <h1>Hello bro</h1>
-      <ThemeToggle />
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      {
+        session ? (
+          <>
+            <h1>Hello {session.user.name}</h1>
+            <ThemeToggle />
+          </>
+        ) : (
+          <div>Not authenticated go to <Link href="/login">login</Link></div>
+        )
+      }
+    </div>
 
   );
 }
