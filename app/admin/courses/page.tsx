b8b35b0698a683d87fd3d7b0723c825fa9d2f407
balinux <ryojuniyantara@gmail.com>
2025-07-28 16:38:11 +0800

    add: empty state component

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -2,22 +2,33 @@ import { buttonVariants } from "@/components/ui/button";
 import { adminGetCources } from "@/data/admin/admin-get-cources";
 import Link from "next/link";
 import AdminCourseCard from "./_components/admin-course-card";
+import EmptyState from "@/components/general/empty-state";
 
 export default async function CoursesPage() {
-    const courses = await adminGetCources()
-    return (
-        <>
-        <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold">Your Courses</h1>
-            <Link className={buttonVariants()} href="/admin/courses/create">
-                Create Course
-            </Link>
+  const courses = await adminGetCources();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link className={buttonVariants()} href="/admin/courses/create">
+          Create Course
+        </Link>
+      </div>
+      {courses.length === 0 ? (
+        <EmptyState
+          title="no courses found"
+          description="Create a course to get started"
+          buttonText="Create a course"
+          href="/admin/courses/create"
+        />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {courses.map((course) => (
+            <AdminCourseCard key={course.id} course={course} />
+          ))}
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
-            {courses.map((course) => (
-                <AdminCourseCard key={course.id} course={course} />
-            ))}
-        </div>
-        </>
-    )
+      )}
+    </>
+  );
 }
+
---
+++ components/general/empty-state.tsx
@@ -0,0 +1,31 @@
+import { Ban, PlusCircle } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+
+interface iAppProps {
+  title: string;
+  description: string;
+  buttonText: string;
+  href: string;
+}
+export default function EmptyState({
+  title,
+  description,
+  buttonText,
+  href,
+}: iAppProps) {
+  return (
+    <div className="flex flex-col flex-1 h-full items-center justify-center rounded-md border-dashed border p-8 text-center animate-in fade-in-50">
+      <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
+        <Ban className="size-10 text-primary" />
+      </div>
+      <h2 className="text-xl mt-6 font-semibold">{title}</h2>p
+      <p className="mb-8 mt-2 text-sm leading-tight text-muted-foreground">
+        {description}
+      </p>
+      <Link href={href} className={buttonVariants()}>
+        <PlusCircle className="mr-2 size-4" /> {buttonText}
+      </Link>
+    </div>
+  );
+}
