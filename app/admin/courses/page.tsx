39ad2092267ae8b593cc7cab0dff6bda2c9864eb
balinux <ryojuniyantara@gmail.com>
2025-07-29 13:24:45 +0800

    feat: skeleton for waiting loading courses

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,11 +1,13 @@
 import { buttonVariants } from "@/components/ui/button";
 import { adminGetCources } from "@/data/admin/admin-get-cources";
 import Link from "next/link";
-import AdminCourseCard from "./_components/admin-course-card";
+import AdminCourseCard, {
+  AdminCourseCardSkeleton,
+} from "./_components/admin-course-card";
 import EmptyState from "@/components/general/empty-state";
+import { Suspense } from "react";
 
-export default async function CoursesPage() {
-  const courses = await adminGetCources();
+export default function CoursesPage() {
   return (
     <>
       <div className="flex items-center justify-between">
@@ -14,6 +16,18 @@ export default async function CoursesPage() {
           Create Course
         </Link>
       </div>
+      <Suspense fallback={<AdminCourseCardSkeletonLayout />}>
+        <RenderCourses />
+      </Suspense>
+    </>
+  );
+}
+
+async function RenderCourses() {
+  const courses = await adminGetCources();
+
+  return (
+    <>
       {courses.length === 0 ? (
         <EmptyState
           title="no courses found"
@@ -32,3 +46,12 @@ export default async function CoursesPage() {
   );
 }
 
+function AdminCourseCardSkeletonLayout() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-7">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <AdminCourseCardSkeleton key={index} />
+      ))}
+    </div>
+  );
+}
--- data/admin/admin-get-cources.ts
+++ data/admin/admin-get-cources.ts
@@ -2,27 +2,31 @@ import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
 export async function adminGetCources() {
-    await requireAdmin()
+  await new Promise((res) => setTimeout(res, 2000));
+  await requireAdmin();
 
-    const data = await prisma.course.findMany({
-        orderBy:{
-            createdAt:"desc"
-        },
-        select:{
-            id:true,
-            title:true,
-            description:true,
-            smallDescription:true,
-            duration:true,
-            level:true,
-            status:true,
-            price:true,
-            fileKey:true,
-            slug:true
-        }
-    })
+  const data = await prisma.course.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      smallDescription: true,
+      duration: true,
+      level: true,
+      status: true,
+      price: true,
+      fileKey: true,
+      slug: true,
+    },
+  });
 
-    return data
+  return data;
 }
 
-export type AdminGetCourcesType = Awaited<ReturnType<typeof adminGetCources>>[0]
+export type AdminGetCourcesType = Awaited<
+  ReturnType<typeof adminGetCources>
+>[0];
+
