647df59e77c8570a1c16f161e7906350b0fb55b3
balinux <ryojuniyantara@gmail.com>
2025-08-07 17:25:03 +0800

    feat: protect delete with arcjet

--- app/admin/courses/[courseid]/delete/action.ts
+++ app/admin/courses/[courseid]/delete/action.ts
@@ -1,13 +1,56 @@
 "use server";
 
 import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
+import { request } from "@arcjet/next";
 import { revalidatePath } from "next/cache";
 
+const aj = arcjet.withRule(
+  detectBot({
+    mode: "LIVE",
+    allow: []
+  })
+)
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5
+    })
+  )
+
+
 export async function deleteCourse(courseId: string): Promise<ApiResponse> {
-  await requireAdmin();
+  const session = await requireAdmin();
   try {
+
+    const req = await request();
+
+    // call arcjet protect
+    const decision = await aj.protect(req, {
+      fingerprint: session?.user.id as string
+    })
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "Too many requests",
+        }
+      } else if (decision.reason.isBot()) {
+        return {
+          status: "error",
+          message: "you are a bot, if you think this is a mistake, please contact support",
+        }
+      } else {
+        return {
+          status: "error",
+          message: "you are a bot, if you think this is a mistake, please contact support",
+        }
+      }
+    }
     await prisma.course.delete({
       where: {
         id: courseId,
