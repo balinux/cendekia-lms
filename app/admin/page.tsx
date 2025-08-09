6995a2c8dbdb3fc09c58e79cc7519a5c70410efc
balinux <ryojuniyantara@gmail.com>
2025-08-09 14:25:19 +0800

    add: analytic - display recent course on dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,12 @@ import { DataTable } from "@/components/sidebar/data-table";
 import { SectionCards } from "@/components/sidebar/section-cards";
 import data from "./data.json"
 import adminGetEnrollmentStats from "../data/admin/admin-get-enrollment-stats";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import adminGetRecentCourse from "../data/admin/admin-get-recent-course";
+import EmptyState from "@/components/general/empty-state";
+import AdminCourseCard, { AdminCourseCardSkeleton } from "./courses/_components/admin-course-card";
+import { Suspense } from "react";
 
 export default async function AdminIndexPage() {
   const enrollmentdata = await adminGetEnrollmentStats()
@@ -12,6 +18,50 @@ export default async function AdminIndexPage() {
       <SectionCards />
       <ChartAreaInteractive data={enrollmentdata} />
       {/* <DataTable data={data} /> */}
+
+      <div className="space-y-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold">Recent Course</h2>
+          <Link
+            className={buttonVariants({
+              variant: "outline",
+            })}
+            href="/admin/course">View All Course</Link>
+        </div>
+        <Suspense fallback={<RenderRecentCourseSkeletonLayout />}>
+          <RenderRecentCourse />
+        </Suspense>
+      </div>
     </>
+  )
+}
+
+async function RenderRecentCourse() {
+  const recentCourse = await adminGetRecentCourse()
+  if (recentCourse.length === 0) {
+    return <EmptyState 
+    title={"no course found"} 
+    description={"create a course to get started"} 
+    buttonText={"create a course"} 
+    href={"/admin/course/create"} />
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {recentCourse.map((course) => (
+        <AdminCourseCard key={course.id} course={course} />
+      ))}
+    </div>
+  )
+}
+
+// skeleton
+function RenderRecentCourseSkeletonLayout() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {Array.from({ length: 3 }).map((_, index) => (
+        <AdminCourseCardSkeleton key={index} />
+      ))}
+    </div>
   )
 }
